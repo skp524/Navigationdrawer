9c63fc6b598acdb889a047088c549df9ec906745
Mickey <shashank.pandey@synlabs.io>
2020-04-03 19:42:52 +0530

    commit

--- app/src/main/java/com/shashank/navigationdrawer/MaintainenceServiceFragment.java
+++ app/src/main/java/com/shashank/navigationdrawer/MaintainenceServiceFragment.java
@@ -1,46 +1,30 @@
 package com.shashank.navigationdrawer;
-
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ListView;
-
 import androidx.fragment.app.Fragment;
-
 import com.shashank.navigationdrawer.adapter.EditTextAdapter;
 import com.shashank.navigationdrawer.model.EditModel;
-
 import java.util.ArrayList;
-
 
 public class MaintainenceServiceFragment extends Fragment
 {
-    private Button btn;
     private ListView lv;
     private EditTextAdapter editTextAdapter;
     public ArrayList<EditModel> editModelArrayList;
-    ListView listView;
 
     String mTitle[] = {"Laptop", "Desktop", "Printer", "Server","Cartridge","Software"};
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_maintainence_service, container, false);
         lv = v.findViewById(R.id.listView);
-        btn = v.findViewById(R.id.btn);
 
         editModelArrayList = populateList();
         editTextAdapter = new EditTextAdapter(this,editModelArrayList);
         lv.setAdapter(editTextAdapter);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
         return v;
 
     }
@@ -49,9 +33,9 @@ public class MaintainenceServiceFragment extends Fragment
 
         ArrayList<EditModel> list = new ArrayList<>();
 
-        for(int i = 0; i < 8; i++){
+        for(int i = 0; i < mTitle.length; i++){
             EditModel editModel = new EditModel();
-            editModel.setEditTextValue(String.valueOf(i));
+            editModel.setTitle(mTitle[i]);
             list.add(editModel);
         }
 
