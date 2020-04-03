71a7639914a2f27a8062795c79c1954b8c97cdd7
Mickey <shashank.pandey@synlabs.io>
2020-04-03 11:10:42 +0530

    commit

--- app/src/main/java/com/shashank/navigationdrawer/MaintainenceServiceFragment.java
+++ app/src/main/java/com/shashank/navigationdrawer/MaintainenceServiceFragment.java
@@ -1,96 +1,61 @@
 package com.shashank.navigationdrawer;
 
 
-import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.shashank.navigationdrawer.adapter.EditTextAdapter;
+import com.shashank.navigationdrawer.model.EditModel;
+
+import java.util.ArrayList;
 
 
 public class MaintainenceServiceFragment extends Fragment
 {
-
+    private Button btn;
+    private ListView lv;
+    private EditTextAdapter editTextAdapter;
+    public ArrayList<EditModel> editModelArrayList;
     ListView listView;
 
     String mTitle[] = {"Laptop", "Desktop", "Printer", "Server","Cartridge","Software"};
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View v=inflater.inflate(R.layout.fragment_maintainence_service, container, false);
+        lv = v.findViewById(R.id.listView);
+        btn = v.findViewById(R.id.btn);
 
-        listView = v.findViewById(R.id.listView);
-        // now create an adapter class
+        editModelArrayList = populateList();
+        editTextAdapter = new EditTextAdapter(this,editModelArrayList);
+        lv.setAdapter(editTextAdapter);
 
-        MyAdapterr adapter = new MyAdapterr(getActivity(), mTitle);
-        listView.setAdapter(adapter);
-        // there is my mistake...
-        // now again check this..
-
-        // now set item click on list view
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position ==  0)
-                {
+            public void onClick(View view) {
 
-                }
-                if (position ==  1) {
-                    Toast.makeText(getContext(), "Whatsapp Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position ==  2) {
-                    Toast.makeText(getContext(), "Twitter Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position ==  3) {
-                    Toast.makeText(getContext(), "Instagram Description", Toast.LENGTH_SHORT).show();
-                }
-                if (position ==  4) {
-                    Toast.makeText(getContext(), "Youtube Description", Toast.LENGTH_SHORT).show();
-                }
             }
         });
-        // so item click is done now check list view
-
-
         return v;
-    }
-}
-
-class MyAdapterr extends ArrayAdapter<String> {
-
-    Context context;
-    String rTitle[];
-
-
-    MyAdapterr(Context c, String title[]) {
-        super(c, R.layout.row, R.id.checkBox, title);
-        this.context = c;
-        this.rTitle = title;
 
     }
 
-    @NonNull
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View row = layoutInflater.inflate(R.layout.row, parent, false);
-        TextView myTitle = row.findViewById(R.id.checkBox);
+    private ArrayList<EditModel> populateList(){
 
-        // now set our resources on views
+        ArrayList<EditModel> list = new ArrayList<>();
 
-        myTitle.setText(rTitle[position]);
+        for(int i = 0; i < 8; i++){
+            EditModel editModel = new EditModel();
+            editModel.setEditTextValue(String.valueOf(i));
+            list.add(editModel);
+        }
 
-
-        return row;
+        return list;
     }
+
 }
